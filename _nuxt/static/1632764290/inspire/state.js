79c7b1abd6a4ac69cb5ef30a87c560769282983d
agam-w <agam-w@users.noreply.github.com>
2021-09-27 18:04:44 +0000

    deploy: 8e186b6a874b3e59f95f29939e5e9dc8ed00c7b9

--- _nuxt/static/1632764290/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsemasa-living\u002F_nuxt\u002Fstatic\u002F1632764290",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fsemasa-living\u002F",assetsPath:"\u002Fsemasa-living\u002F_nuxt\u002F",cdnURL:a}}}}(null));
